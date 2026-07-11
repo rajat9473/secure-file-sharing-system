0ffe27176da5be5141599a95a3a960c06d9653b5
Rajat Chitransh <rajatchitransh02@gmail.com>
2026-07-12 00:01:52 +0530

    Fix API URL for production

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:5000/api",
 });
 
 export default API;
